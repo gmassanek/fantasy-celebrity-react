4a128e13a9c1968b9aa38a21219128fc69d178ff
Geoff Massanek <gmassanek@gmail.com>
2015-08-02 17:50:03 -0500

    Move things around to get styles to match MVP template

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,5 @@
 var gulp = require('gulp');
+var sass = require('gulp-sass');
 var bower = require('gulp-bower')
 var browserify = require('browserify');
 var babelify = require('babelify');
@@ -17,6 +18,28 @@ gulp.task('vendorJS', function(){
   .pipe(gulp.dest('./public/assets/'));
 });
 
+gulp.task('vendorCSS', function(){
+  gulp.src([
+    './vendor/mvpready-admin.css'
+  ])
+  .pipe(sass().on('error', sass.logError))
+  .pipe(concat('vendor.css'))
+  .pipe(gulp.dest('public/assets'));
+});
+
+gulp.task('watchSass', function() {
+  gulp.watch('src/**/*.scss', ['sass']);
+});
+
+gulp.task('sass', function(){
+  gulp.src([
+    'src/application.scss'
+  ])
+  .pipe(sass().on('error', sass.logError))
+  .pipe(concat('application.css'))
+  .pipe(gulp.dest('public/assets'));
+});
+
 var srcPath = 'src/application.js';
 gulp.task('build', function () {
   browserify({
@@ -30,8 +53,8 @@ gulp.task('build', function () {
   .pipe(gulp.dest('public/assets'));
 });
 
-gulp.task('watch', function() {
+gulp.task('watchJs', function() {
   gulp.watch('src/**/*.js', ['build']);
 });
 
-gulp.task('default', ['vendorJS', 'build', 'watch']);
+gulp.task('default', ['vendorJS', 'sass', 'build', 'watchJs', 'watchSass']);
--- src/application.js
+++ src/application.js
@@ -6,13 +6,163 @@ var App = React.createClass({
   render () {
     return (
       <div>
-        <h1>App</h1>
-        <ul>
-          <li><Link to="/hello">About</Link></li>
-          <li><Link to="/world">Inbox</Link></li>
-        </ul>
+        <div id="wrapper">
+          <header className="navbar" role="banner">
+            <div className="container">
+              <div className="navbar-header">
+                <button className="navbar-toggle" type="button" data-toggle="collapse" data-target=".navbar-collapse">
+                  <span className="sr-only">Toggle navigation</span>
+                  <i className="fa fa-cog"></i>
+                </button>
 
-        {this.props.children}
+                <a href="./" className="navbar-brand navbar-brand-img">
+                  FantasyCeleb
+                </a>
+              </div>
+
+              <nav className="collapse navbar-collapse" role="navigation">
+                <ul className="nav navbar-nav navbar-right">
+
+                  <li>
+                    <a href="javascript:;">About</a>
+                  </li>
+
+                  <li className="dropdown navbar-profile">
+                    <a className="dropdown-toggle" data-toggle="dropdown" href="javascript:;">
+                      <img src="https://avatars1.githubusercontent.com/u/343891?v=3&s=40" className="navbar-profile-avatar" alt=""/>
+                      <span className="visible-xs-inline">@peterlandt &nbsp;</span>
+                      <i className="fa fa-caret-down"></i>
+                    </a>
+
+                    <ul className="dropdown-menu" role="menu">
+
+                      <li>
+                        <a href="./page-profile.html">
+                          <i className="fa fa-user"></i>
+                          &nbsp;&nbsp;My Profile
+                        </a>
+                      </li>
+
+                      <li>
+                        <a href="./page-settings.html">
+                          <i className="fa fa-cogs"></i>
+                          &nbsp;&nbsp;Settings
+                        </a>
+                      </li>
+
+                      <li className="divider"></li>
+
+                      <li>
+                        <a href="./account-login.html">
+                          <i className="fa fa-sign-out"></i>
+                          &nbsp;&nbsp;Logout
+                        </a>
+                      </li>
+
+                      <li className="divider"></li>
+
+                      <li>
+                        <a href="/leagues/1">
+                          <i className="fa fa-bars dropdown-icon "></i>
+                          League 1
+                        </a>
+                      </li>
+                      <li>
+                        <a href="/leagues/2">
+                          <i className="fa fa-bars dropdown-icon "></i>
+                          League 2
+                        </a>
+                      </li>
+                      <li>
+                        <a href="/leagues/3">
+                          <i className="fa fa-bars dropdown-icon "></i>
+                          League 3
+                        </a>
+                      </li>
+
+                    </ul>
+
+                  </li>
+
+                </ul>
+              </nav>
+            </div>
+          </header>
+          <div className="mainnav">
+            <div className="container">
+              <a className="mainnav-toggle" data-toggle="collapse" data-target=".mainnav-collapse">
+                <span className="sr-only">Toggle navigation</span>
+                <i className="fa fa-bars"></i>
+              </a>
+
+              <nav className="collapse mainnav-collapse" role="navigation">
+                <ul className="mainnav-menu">
+                  <li className="dropdown is-open">
+                    <a href="/leagues/{{ model.id }}" className="dropdown-toggle" data-toggle="dropdown" data-hover="dropdown">
+                      League
+                      <i className="mainnav-caret"></i>
+                    </a>
+
+                    <ul className="dropdown-menu" role="menu">
+                      <li>
+                        <a href="/leagues/{{ model.id }}">
+                          <i className="fa fa-dashboard dropdown-icon "></i>
+                          Dashboard
+                        </a>
+                      </li>
+
+                      <li>
+                        <a href="/leagues/{{ model.id }}/positions">
+                          <i className="fa fa-dashboard dropdown-icon "></i>
+                          Positions
+                        </a>
+                      </li>
+
+                      <li>
+                        <a href="/leagues/{{ model.id }}/scoring">
+                          <i className="fa fa-dashboard dropdown-icon "></i>
+                          Scoring
+                        </a>
+                      </li>
+                    </ul>
+                  </li>
+
+                  <li>
+                    <a href="/leagues/{{ model.id }}/teams/2"> My Team </a>
+                  </li>
+
+                  <li>
+                    <a href="leagues/{{ model.id }}/players">
+                      Players
+                    </a>
+                  </li>
+
+                  <li>
+                    <a href="leagues/{{ model.id }}/points">
+                      Points
+                    </a>
+                  </li>
+
+                  <li>
+                    <a href="leagues/{{ model.id }}/standings">
+                      Standings
+                    </a>
+                  </li>
+                </ul>
+              </nav>
+            </div>
+          </div>
+          <div className="content">
+            <div className="container">
+              {this.props.children}
+            </div>
+          </div>
+        </div>
+        <footer className="footer">
+          <div className="container">
+            <p className="pull-left">Copyright &copy; Geoff.</p>
+          </div>
+        </footer>
       </div>
     )
   }
